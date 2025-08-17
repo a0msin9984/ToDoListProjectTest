7e8b9c1766b030bae2db546636449d274cc19c21
Thanaphat Satitkunrus <satitkunrus.t@gmail.com>
2025-08-17 20:51:41 +0700

    feat: Can check what task success

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,6 +33,14 @@ class TasksController < ApplicationController
     end
   end
 
+  def toggle
+    @task = Task.find(params[:id])
+    @task.update(completed: !@task.completed)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+    end
+  end
+
   private
     def task_params
       params.require(:task).permit(:title)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "homepage#index"
-  resources :tasks, only: [ :index, :create, :update, :destroy ]
+
+  resources :tasks, only: [:index, :create, :update, :destroy] do
+    member do
+      patch :toggle
+    end
+  end
+
   post "/", to: "homepage#create"  # POST /
   get "up" => "rails/health#show", as: :rails_health_check
 end
